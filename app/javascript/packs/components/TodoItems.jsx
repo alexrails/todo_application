e3204ed0424daf5dcf3e69314a6cd94df00aa0dc
Alex <alexrails@yandex.ru>
2022-01-04 15:57:31 +0300

    add filtering

--- app/javascript/packs/components/TodoItems.jsx
+++ app/javascript/packs/components/TodoItems.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 class TodoItems extends React.Component {
   constructor(props) {
     super(props)
+    this.handleClick = this.handleClick.bind(this)
+  }
+  handleClick() {
+    this.props.toggleCompletedTodoItems()
   }
   render() {
     return (
       <>
+        <hr />
+        <button className="btn btn-outline-primary btn-block mb-3" onClick={this.handleClick}>
+          {this.props.hideCompletedTodoItems ? `Show Completed Items` : `Hide Completed Items `}
+        </button>
         <div className="table-responsive">
           <table className="table">
             <thead>
