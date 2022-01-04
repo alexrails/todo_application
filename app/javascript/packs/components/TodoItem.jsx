ec00c17adeb25be287fbee0da1ad94aaabfc4b1d
Alex <alexrails@yandex.ru>
2022-01-04 15:43:47 +0300

    add lodash

--- app/javascript/packs/components/TodoItem.jsx
+++ app/javascript/packs/components/TodoItem.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import _ from 'lodash'
 import axios from 'axios'
 import setAxiosHeaders from './AxiosHeaders'
 class TodoItem extends React.Component {
@@ -17,10 +18,12 @@ class TodoItem extends React.Component {
     this.path = `/api/v1/todo_items/${this.props.todoItem.id}`
   }
   handleChange() {
+    this.setState({
+      complete: this.completedRef.current.checked
+    });
     this.updateTodoItem();
   }
-  updateTodoItem() {
-    this.setState({ complete: this.completedRef.current.checked });
+  updateTodoItem = _.debounce(() => {
     setAxiosHeaders();
     axios
       .put(this.path, {
@@ -33,7 +36,8 @@ class TodoItem extends React.Component {
       .catch(error => {
         console.log(error);
       });
-  }
+  }, 1000);
+  
   handleDestroy() {
     setAxiosHeaders();
     const confirmation = confirm("Are you sure?");
